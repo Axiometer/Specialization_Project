7de57b6f1ad5f48285192ed4ac2233a9aa278000
Axiometer <mc707.int3@gmail.com>
2023-03-14 18:57:41 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string[] M;
+Console.WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine();
+M = stroka.Split(' ');
+var result = new string[M.Length];
+var realSize = 0;
+foreach (var value in M )
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+
+System.Console.WriteLine("\nВывод всех слов длиннее 3:");
+foreach (var i in result)
+    System.Console.Write(i+" ");
