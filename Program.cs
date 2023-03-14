d7b12d9088da3620f71d648776f3d19e3ff0cfbd
Axiometer <mc707.int3@gmail.com>
2023-03-14 19:11:42 +0300

    добавление комментариев

--- Program.cs
+++ Program.cs
@@ -1,18 +1,26 @@
-﻿string[] M;
+﻿// задаем массив строк
+string[] inputString;
 Console.WriteLine("Введите массив строк через пробел");
-string stroka = Console.ReadLine();
-M = stroka.Split(' ');
-var result = new string[M.Length];
+// вводим его с клавиатуры, разбивая в массив по пробелам
+inputString = Console.ReadLine().Split(' ');
+// создаем новый массив такой же размерности как получившийся
+var result = new string[inputString.Length];
+// текущая размерность нового массива
 var realSize = 0;
-foreach (var value in M )
+
+// пробегаем в цикле по изначальному массиву строк
+foreach (var value in inputString)
 {
+    // если длина строки текущей итерации не превышает 3
     if (value.Length <= 3)
     {
+        // добавляем это слово в новый массив и прибавляем счетчик размерности
         result[realSize] = value;
         realSize++;
     }
 }
 
+// выводим результат на экран
 System.Console.WriteLine("\nВывод всех слов длиннее 3:");
 foreach (var i in result)
     System.Console.Write(i+" ");
